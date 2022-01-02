1847c1dd1432053452c446bc12eb1ea4ecb2cf2d
Marc Hermans <marc.hermans@ldtteam.com>
2022-01-02 15:09:44 +0100

    Initial cleanup pass.

--- Executor/src/main/java/net/minecraftforge/ducker/executor/ExecutorService.java
+++ Executor/src/main/java/net/minecraftforge/ducker/executor/ExecutorService.java
@@ -1,28 +1,11 @@
 package net.minecraftforge.ducker.executor;
 
-import com.google.common.collect.Lists;
-import com.google.common.reflect.ClassPath;
 import net.minecraftforge.ducker.configuration.DuckerConfiguration;
-import net.minecraftforge.ducker.mixin.DuckerExecutor;
 import net.minecraftforge.ducker.mixin.DuckerExecutorMixinService;
 import net.minecraftforge.ducker.mixin.DuckerMixinApplier;
 import net.minecraftforge.ducker.mixin.DuckerMixinBootstrap;
-import net.minecraftforge.ducker.mixin.classes.IClassProcessor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.objectweb.asm.Type;
-import org.objectweb.asm.tree.ClassNode;
-import org.spongepowered.asm.launch.MixinBootstrap;
-import org.spongepowered.asm.service.IMixinService;
-import org.spongepowered.asm.service.MixinService;
-
-import java.io.File;
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URL;
-import java.util.List;
-import java.util.Objects;
 
 /**
  * Executes
