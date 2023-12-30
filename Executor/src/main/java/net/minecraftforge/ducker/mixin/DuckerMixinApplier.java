290d143464c78af3c36ca264cbc0e8f536654439
TheJudge <colepatrickroberts@gmail.com>
2023-12-29 18:51:29 -0800

    Fix inner classes

--- Executor/src/main/java/net/minecraftforge/ducker/mixin/DuckerMixinApplier.java
+++ Executor/src/main/java/net/minecraftforge/ducker/mixin/DuckerMixinApplier.java
@@ -8,8 +8,10 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.objectweb.asm.Type;
 import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.InnerClassNode;
 
 import java.io.IOException;
+import java.util.List;
 
 public class DuckerMixinApplier
 {
@@ -32,18 +34,15 @@ public class DuckerMixinApplier
                     {
                         final ClassNode classNode = duckerExecutorMixinService.getBytecodeProvider().getClassNode(classInfo.getName(), true);
                         System.out.println("We're now at " + classNode.name);
-                        if (processClassNode(duckerExecutorMixinService, classNode)) {
-                            classNode.sourceDebug = null;
 
-                            System.out.println("Processed " + classNode.name);
-                            LOGGER.info("Processed {}", classNode.name);
-                            final IClassWriter.ClassWriterResult writerResult = duckerConfiguration.getClassWriter().writeClass(duckerConfiguration, classNode, duckerExecutorMixinService);
-                            System.out.println("Written class " + classInfo.getName());
-                            LOGGER.info("Written class {}, to {}, with {} additional files", classNode.name, writerResult.file().getAbsolutePath(), writerResult.additionalFiles().size());
-                            duckerConfiguration.getDecompiler().decompile(writerResult.file(), writerResult.additionalFiles());
-                            System.out.println("Decompiled " + classNode.name);
-                            LOGGER.info("Decompiled class {}.", classNode.name);
+                        for(InnerClassNode innerNode : classNode.innerClasses) {
+                            System.out.println("Also processing inner class: " + innerNode.name);
+                            final ClassNode innerNodeClazz = duckerExecutorMixinService.getBytecodeProvider().getClassNode(innerNode.name, true);
+
+                            processClass(duckerConfiguration, duckerExecutorMixinService, classInfo, innerNodeClazz);
                         }
+
+                        processClass(duckerConfiguration, duckerExecutorMixinService, classInfo, classNode);
                     }
                     catch (ClassNotFoundException | IOException e)
                     {
@@ -58,6 +57,21 @@ public class DuckerMixinApplier
         }
     }
 
+    private static void processClass(DuckerConfiguration duckerConfiguration, DuckerExecutorMixinService duckerExecutorMixinService, ClassPath.ClassInfo classInfo, ClassNode classNode) {
+        if (processClassNode(duckerExecutorMixinService, classNode)) {
+            classNode.sourceDebug = null;
+
+            System.out.println("Processed " + classNode.name);
+            LOGGER.info("Processed {}", classNode.name);
+            final IClassWriter.ClassWriterResult writerResult = duckerConfiguration.getClassWriter().writeClass(duckerConfiguration, classNode, duckerExecutorMixinService);
+            System.out.println("Written class " + classInfo.getName());
+            LOGGER.info("Written class {}, to {}, with {} additional files", classNode.name, writerResult.file().getAbsolutePath(), writerResult.additionalFiles().size());
+            duckerConfiguration.getDecompiler().decompile(writerResult.file(), writerResult.additionalFiles());
+            System.out.println("Decompiled " + classNode.name);
+            LOGGER.info("Decompiled class {}.", classNode.name);
+        }
+    }
+
     private static boolean processClassNode(final DuckerExecutorMixinService duckerExecutorMixinService, final ClassNode classNode) {
         boolean processed = false;
 
@@ -68,5 +82,4 @@ public class DuckerMixinApplier
 
         return processed;
     }
-
 }
